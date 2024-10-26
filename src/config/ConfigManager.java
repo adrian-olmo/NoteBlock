c8c57d8537a8d5abd3c69d3a8581f15070e961ce
Adrian Olmo <47026705+adrian-olmo@users.noreply.github.com>
2024-10-26 04:02:55 +0200

    added Config Manager
    Implementación de la clase ConfigManager para cargar propiedades desde un archivo.
    Esta clase permite la carga de configuraciones desde un archivo especificado en el constructor
    y proporciona un método para acceder a las propiedades mediante una clave.

--- src/config/ConfigManager.java
+++ src/config/ConfigManager.java
@@ -1,4 +1,23 @@
 package config;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+import java.io.FileInputStream;
+
 public class ConfigManager {
+    private final Properties properties;
+
+    public ConfigManager(String propertiesFilePath) {
+        properties = new Properties();
+        try ( InputStream input = new FileInputStream(propertiesFilePath)) {
+            properties.load(input);
+        } catch ( IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String getProperty(String key) {
+        return properties.getProperty(key);
+    }
 }
