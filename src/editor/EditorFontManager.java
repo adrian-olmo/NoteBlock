062f9cd7bcd3273f25380a621a1f12625ff11b0d
Adrian Olmo <47026705+adrian-olmo@users.noreply.github.com>
2024-10-26 04:29:00 +0200

    Added functions
    Se agregan las funciones para cambiar la fuente y el tamaño

--- src/editor/EditorFontManager.java
+++ src/editor/EditorFontManager.java
@@ -1,4 +1,28 @@
 package editor;
 
+import java.awt.*;
+import ui.NoteBlock;
+
 public class EditorFontManager {
+
+    public void changeFontSize() {
+        for (int i = 20; i <= 50; i++) {
+            NoteBlock.sizeModel.addElement(i);
+        }
+    }
+
+    public void changeFontFamily() {
+        GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+        String[] fonts = ge.getAvailableFontFamilyNames();
+        for (String font : fonts) {
+            NoteBlock.fontModel.addElement(font);
+        }
+    }
+
+    public void editFont() {
+        Font font = new Font((String) NoteBlock.fontFamily.getSelectedItem(),
+                Font.PLAIN, (int)
+                NoteBlock.fontSize.getSelectedItem());
+        NoteBlock.textArea.setFont(font);
+    }
 }
